a080a6681d4ba1820e47a0670bd58d745382b073
whaleand <whaleand2022@163.com>
2022-08-18 15:55:55 +0800

    完成了商品列表开发

---
+++ components/my-goods/my-goods.vue
@@ -0,0 +1,75 @@
+<template>
+	<view>
+		<view class="goods-item">
+			<!-- 左边部分 -->
+			<view class="goods-item-left">
+				<image :src="goods.goods_small_logo||defaultUrl" class="goods-pic"></image>
+			</view>
+			<!-- 右侧部分 -->
+			<view class="goods-item-right">
+				<view class="goods-name">
+					{{goods.goods_name}}
+				</view>
+				<view class="goods-info-box">
+					<view class="goods-price">
+						￥{{goods.goods_price|tofixed}}
+					</view>
+				</view>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		name:"my-goods",
+		data() {
+			return {
+				defaultUrl:'/static/defaltPic.gif'
+				
+			};
+		},
+			
+		props:{
+			goods:{
+				type:Object,
+				default:{}
+			}
+		},
+		filters:{
+			// 处理商品价格,保留两位小数
+			tofixed(num){
+				return Number(num).toFixed(2)
+			}
+		}
+	}
+</script>
+
+<style lang="scss">
+.goods-item{
+	display: flex;
+	padding: 5px;
+	border-bottom: 1px solid #f0f0f0;
+	.goods-item-left{
+		.goods-pic{
+			width: 100px;
+			height: 100px;
+			display: block;
+		}
+		margin-right: 5px;
+	}
+	.goods-item-right{
+		display: flex;
+		flex-direction: column;
+		justify-content: space-between;
+		.goods-name{
+			font-size: 13px;
+		}
+			
+		.goods-price{
+			font-size: 16px;
+			color: #c00000;
+		}
+	}
+}
+</style>
--- subpkg/goods_list/goods_list.vue
+++ subpkg/goods_list/goods_list.vue
@@ -1,6 +1,11 @@
 <template>
 	<view>
+	<view class="goods-list">
+		<view v-for="(goods,i) in GoodsList" :key="i" @click="gotoDetail(goods)">
+			<my-goods :goods=goods></my-goods>
+		</view>
 		
+	</view>
 	</view>
 </template>
 
@@ -8,12 +13,61 @@
 	export default {
 		data() {
 			return {
-				
+				queryObj: {
+					query: '',
+					cid: '',
+					pagenum: 1,
+					pagesize: 10
+				},
+				GoodsList: [],
+				total: 0,
+				// 节流阀
+				isLoading:false,
 			}
 		},
 		methods: {
-			
-		}
+			async getGoodsList(cb) {
+				this.isLoading=true
+				const {
+					data: res
+				} = await uni.$http.get('/api/public/v1/goods/search', this.queryObj)
+				this.isLoading=false
+				//判断回调  函数
+				cb&&cb()
+				if (res.meta.status !== 200) return uni.$showMsg()
+				this.GoodsList = [...this.GoodsList,...res.message.goods]
+				this.total = res.message.total
+			},
+			gotoDetail(item){
+				uni.navigateTo({
+					url:'/subpkg/goods_detail/goods_detail?goods_id='+item.goods_id
+				})
+			}
+		},
+		onLoad(options) {
+			this.queryObj.query = options.query || ''
+			this.queryObj.cid = options.cid || ''
+			this.getGoodsList()
+		},
+		onReachBottom(){
+			if(this.queryObj.pagenum*this.queryObj.pagesize>=this.total) return uni.$showMsg('数据加载完毕!')
+			if(this.isLoading) return
+			this.queryObj.pagenum++
+			this.getGoodsList()
+		},
+		onPullDownRefresh(){
+			//重置关键数据
+			this.queryObj.pagenum=1
+			this.total=0
+			this.isLoading=false
+			this.GoodsList=[]
+			// 重新发起数据请求
+			this.getGoodsList(()=>{
+				//停止下拉刷新 
+				uni.stopPullDownRefresh()
+			})
+		},
+		
 	}
 </script>
 
